73de18e31fb048610ad28c3d0328de02aec478a4
birolezgin <b.ezginksf@gmail.com>
2020-11-07 21:17:33 -0800

    Footer.js generated

---
+++ reactportfolio/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  
+    return (
+        <footer className="page-footer font-small pt-4">
+        <div className="footer-copyright text-center py-3">
+            © Copyright Birol Ezgin 2020
+        </div>
+    </footer>
+    );
+  }
+  
+  export default Footer;
